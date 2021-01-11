5caa1edd6cbc79b3997170aca7e422ebcd1280d0
Andres Ruiz <andres87aldos@gmail.com>
2021-01-11 10:11:32 -0600

    Clase 7

---
+++ variables.php
@@ -0,0 +1,18 @@
+<?php
+
+//Asignacion
+$num = 9;
+$lang = [
+    'es' => 'español',
+    'en' => 'english'
+];
+//Aritmetica
+echo "Suma 2 + 2 " . ((int) 2 + (int)2);
+echo "REsta 2 + 2 " . ((int) 2 - (int)2);
+echo ",ultiplica 2 * 2 " . 2 * 2;
+echo "Divide 2 / 2 " . 2 / 2;
+echo "Modulo 2 % 2 " . 2 % 2;
+echo "Exponencial 2 ** 2 " . 2 ** 2;
+
+
+
